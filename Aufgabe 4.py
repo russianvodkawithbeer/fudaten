5233b3fa84f1dba3fd1a7589979a8a05d2758641
russianvodkawithbeer <xomabor@gmail.com>
2017-06-08 23:33:23 +0200

    Update Aufgabe 4.py

--- Aufgabe 4.py
+++ Aufgabe 4.py
@@ -8,12 +8,16 @@ import string
 def link(url):
     html = requests.get(url).text
     soup = BeautifulSoup(html, "html.parser")
+    #legen zähler fest
     zaehler = Counter()
     for bible_text in soup.findAll('header'):
+        #löscht paar unnötige sachen
         text = re.sub(r"(?m)^[^\S\n]+", "", bible_text.get_text().lower())
         zaehler.update(text.split(" "))
     return zaehler
 
+# Der link aus der Aufgabe
 zaehler1 = link('https://www.heise.de/thema/https')
 
+#Ausgabe
 print(zaehler1.most_common(3))
